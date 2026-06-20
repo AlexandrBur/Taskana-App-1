34515dda6edfedb7c603682db9c065bde23d9bc0
Alexandr <123650995+AlexandrBur@users.noreply.github.com>
2026-06-20 15:42:35 +0300

    Modify base URL in vite.config.js
    Update base URL configuration to use GITHUB_REPOSITORY environment variable.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Taskana-App-1/',
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
